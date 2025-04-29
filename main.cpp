0f2f5bc50286070b14edb2fb2a5cb29fbfa345cf
Ha Trung Thanh <hathanh24dt@gmail.com>
2025-04-29 11:40:33 +0700

    day code

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Khai báo 3 số nguyên
+    int a = 5, b = 3, c = 2;
+
+    // Tính tổng và hiệu
+    int sum = a + b + c;
+    int diff = a - b - c;
+
+    // In ra kết quả
+    cout << "Tong: " << sum << endl;
+    cout << "Hieu: " << diff << endl;
+
+    return 0;
+}
